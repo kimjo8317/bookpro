6ae11fe5ad2e48c09e17a0477bd7ece85e00b1b0
kimjunoh <kimjo8317@gmail.com>
2023-06-20 17:26:46 +0900

    책제목으로 책정보조회 기능

--- src/main/java/com/book/project/domain/Controller/BookInfoController.java
+++ src/main/java/com/book/project/domain/Controller/BookInfoController.java
@@ -5,6 +5,7 @@ import com.book.project.domain.Repository.BookInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,5 +24,10 @@ public class BookInfoController {
     @GetMapping("/books")
     public List<BookInfoEntity> getAllBooks() {
         return bookInfoRepository.findAll();
+    }
+
+    @GetMapping("/books/search")
+    public List<BookInfoEntity> searchBooksByTitle(@RequestParam("title") String title) {
+        return bookInfoRepository.findByTitleContaining(title);
     }
 }
--- src/main/java/com/book/project/domain/Repository/BookInfoRepository.java
+++ src/main/java/com/book/project/domain/Repository/BookInfoRepository.java
@@ -4,8 +4,14 @@ import com.book.project.domain.Entity.BookInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface BookInfoRepository extends JpaRepository<BookInfoEntity, Long> {
     BookInfoEntity save(BookInfoEntity entity);
+
+    //제목으로 검색
+    List<BookInfoEntity> findByTitleContaining(String title);
+
 }
